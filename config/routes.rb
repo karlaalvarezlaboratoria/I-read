a29ab29fd8df938b1ec0ddc83227208b23a5472d
Karla Álvarez <karla.alvarez@michelada.io>
2020-05-28 13:23:57 -0500

    Add Log out button

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,4 @@ Rails.application.routes.draw do
   devise_scope :account do
     post '/sign_up_validation', to: 'accounts/omniauth_callbacks#sign_up_validation'
   end
-
-  # authenticated :account do
-  #   root 'home#new' # , as: :authenticated_root
-  # end
-
-  # unauthenticated :account do
-  # root to: 'public#index' # , as: :unauthenticated_root
-  # end
 end
