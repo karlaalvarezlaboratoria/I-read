c309aabd782d162a3320f1c8b96b80b685279e39
Karla Álvarez <karla.alvarez@michelada.io>
2020-05-29 14:17:58 -0500

    Update test, have an error

--- test/integration/user_can_create_an_account_test.rb
+++ test/integration/user_can_create_an_account_test.rb
@@ -3,8 +3,10 @@
 require 'test_helper'
 
 class UserCanCreateAnAccountTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   def setup
     @account = accounts(:one)
+    # request.env['devise.mapping'] = Devise.mappings[:accounts]
   end
 
   test 'should get new' do
@@ -17,7 +19,7 @@ class UserCanCreateAnAccountTest < ActionDispatch::IntegrationTest
       post account_registration_url, params: { account: { email: 'otro@example.com', password: 'secret', password_confirmation: 'secret' } }
     end
 
-    assert_redirected_to root_url # (User.last)
+    assert_redirected_to home_url # (User.last)
   end
 
   # ESTE NECESITA LOGIN PREVIO, ES NECESARIO TESTEAR???
@@ -27,18 +29,17 @@ class UserCanCreateAnAccountTest < ActionDispatch::IntegrationTest
   #   assert_response :success
   # end
 
-  test 'should update user' do
-    patch account_registration_url(@account), params: { account: { email: @account.email, password: 'otro', password_confirmation: 'otro' } }
-    # A DÓNDE VA A REDIRECCIONAR???
-    # assert_redirected_to account_url # (@user)
-  end
+  # test 'should update account' do
+  #   patch account_registration_url(@account), params: { account: { email: @account.email, password: 'otro', password_confirmation: 'otro' } }
+  #   # A DoNDE VA A REDIRECCIONAR???
+  #   # assert_redirected_to account_url # (@user)
+  # end
 
-  #ESTE NO ESTÁ PASANDO
-  test 'should destroy user' do
+  # ESTE NO ESTa PASANDO
+  test 'should destroy account' do
+    sign_in @account
     assert_difference('Account.count', -1) do
       delete account_registration_url(@account)
     end
-    # A DÓNDE VA A REDIRECCIONAR???
-    # assert_redirected_to account_url
   end
 end
