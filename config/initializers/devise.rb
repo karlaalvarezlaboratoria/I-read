1a2e2b5dcac83609ce19ef0cce19b9f590838a53
Karla Álvarez <karla.alvarez@michelada.io>
2020-06-01 11:28:22 -0500

    Update URL callback for facebook login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, Figaro.env.FACEBOOK_APP_ID, Figaro.env.FACEBOOK_APP_SECRET, callback_url: 'http://localhost:3000/accounts/auth/facebook/callback'
+  config.omniauth :facebook, Figaro.env.FACEBOOK_APP_ID, Figaro.env.FACEBOOK_APP_SECRET, callback_url: Figaro.env.URL_CALLBACK
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
