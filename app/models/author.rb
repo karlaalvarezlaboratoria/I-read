d910302163a2ffd18c92e59f1cbb8414de9e57be
Karla Álvarez <karla.alvarez@michelada.io>
2020-06-05 00:22:34 -0500

    Add validations to Author and Publisher. Update fixtures

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Author < ApplicationRecord
+  validates :name, uniqueness: true, presence: true
+  validates :born, :died, format: {
+    with: /(\A[12]\d{3})|\A\z/i,
+    message: 'add a valid year'
+  }
 end
