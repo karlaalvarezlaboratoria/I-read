a1010e7498fcf11319d83c012e95e9dc8f4f8138
Karla Álvarez <karla.alvarez@michelada.io>
2020-06-10 15:42:21 -0500

    Delete empty files and dead code

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Account < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable,
          omniauth_providers: [:facebook]
