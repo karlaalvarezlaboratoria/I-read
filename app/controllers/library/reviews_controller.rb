ac2ce7965cf140262629ae98118fb6f5200cd6f3
Karla Álvarez <karla.alvarez@michelada.io>
2020-06-12 12:33:17 -0500

    Add validates for unique review

--- app/controllers/library/reviews_controller.rb
+++ app/controllers/library/reviews_controller.rb
@@ -4,6 +4,7 @@ module Library
   class ReviewsController < ApplicationController
     before_action :authenticate_account!
     before_action :find_review, only: %i[edit update destroy]
+    # before_action :user_review, only: %i[edit]
 
     rescue_from 'ActiveRecord::RecordNotFound' do |exception|
       render html: exception, status: 404
@@ -16,7 +17,12 @@ module Library
     def show; end
 
     def new
-      @review = book.reviews.build
+      if user_have_review?
+        @review = user_review
+        redirect_to edit_library_book_review_path(@book, @review)
+      else
+        @review = book.reviews.build
+      end
     end
 
     def create
@@ -36,6 +42,11 @@ module Library
       respond_with :library, book, @review
     end
 
+    def user_have_review?
+      (current_account.reviews.pluck :book_id).include?(book[:id])
+    end
+    helper_method :user_have_review?
+
     private
 
     def review_params
@@ -50,5 +61,13 @@ module Library
       @book = Book.find(params[:book_id])
     end
     helper_method :book
+
+    def user_have_review?
+      (current_account.reviews.pluck :book_id).include?(book[:id])
+    end
+
+    def user_review
+      @review = current_account.reviews.find_by(book_id: book[:id])
+    end
   end
 end
