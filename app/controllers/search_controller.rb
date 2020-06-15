b38e7b25494bf2ec3378886d10068ba6c67ac2e7
Karla Álvarez <karla.alvarez@michelada.io>
2020-06-15 18:05:32 -0500

    Add test to search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,6 @@ class SearchController < ApplicationController
     word = "%#{params[:keyword]}%"
     books = Book.where('title ILIKE ? OR description ILIKE ?', word, word)
     author = Author.find_by('name ILIKE ?', word)
-
     @search = author ? books + author.books : books
 
     respond_with @search
